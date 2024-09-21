5e6f6e178542dcf530c6c84992e5d9eaf2249b31
mahfuj <mahfujahmad424@gmail.com>
2024-09-21 22:07:24 +0600

    final commit

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_apiKey,
   authDomain:import.meta.env.VITE_authDomain,
@@ -13,6 +13,7 @@ const firebaseConfig = {
   appId:import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
+
+
 const app = initializeApp(firebaseConfig);
 export default app;
